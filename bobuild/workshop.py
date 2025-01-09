1fa7d3e3cab5cc8cce596a31a0258cb83d01c6a8
Tuomo Kriikkula <tuokri@tuta.io>
2025-01-10 01:28:47 +0200

    Adjust map name text dynamically, better bg glow drawing

--- bobuild/workshop.py
+++ bobuild/workshop.py
@@ -5,11 +5,22 @@ from PIL import ImageDraw
 from PIL import ImageFilter
 from PIL import ImageFont
 
+from bobuild.log import logger
+
 _file_dir = Path(__file__).parent.resolve()
 _repo_dir = _file_dir.parent
-_draw_y = 880
 _glow_x = 4
 _glow_y = 4
+
+
+def linear_convert(
+        value: float,
+        old_min: float,
+        old_max: float,
+        new_min: float,
+        new_max: float,
+) -> float:
+    return ((value - old_min) / (old_max - old_min)) * (new_max - new_min) + new_min
 
 
 def draw_map_preview_file(
@@ -21,13 +32,32 @@ def draw_map_preview_file(
 ):
     img = Image.open(template_file)
     iw, ih = img.size
-    font = ImageFont.truetype(str(font_file.resolve()), 100)
+
+    # Font size 90 is good for max 22 characters.
+    # For size 59 we can fill up to 36 characters.
+    # NOTE: this is just a rough estimate since we are not using a monospace font!
+    font_size = 90
+    l = len(map_name)
+    if l > 36:
+        logger.warning("map name longer than 36 characters: {}", l)
+    if l > 22:
+        # Swap old_min and old_max for "linear inversion".
+        font_size = int(linear_convert(l, 36, 22, 59, 90))
+    font = ImageFont.truetype(str(font_file.resolve()), font_size)
 
     draw = ImageDraw.Draw(img)
     glow_img = Image.new("RGBA", (iw, ih), color=(0, 0, 0))
     draw_glow = ImageDraw.Draw(glow_img)
 
     _, _, w, h = draw.textbbox((0, _draw_y), map_name, font=font)
+    if w > iw:
+        delta = (w - iw) / iw
+        font_size = int((1 - delta) * font_size)
+        logger.info("map name exceeds image width, setting new font size: {}",
+                    font_size)
+        font = ImageFont.truetype(str(font_file.resolve()), font_size)
+        _, _, w, h = draw.textbbox((0, _draw_y), map_name, font=font)
+
     text_x = (iw - w) / 2
     text_y = ((ih - h) / 2) + _draw_y
 
@@ -37,24 +67,28 @@ def draw_map_preview_file(
         text=map_name,
         font=font,
         fill=(255, 255, 255),
+        stroke_width=2,
     )
     draw_glow.text(
         (text_x - _glow_x, text_y - _glow_y),
         text=map_name,
         font=font,
         fill=(255, 255, 255),
+        stroke_width=2,
     )
     draw_glow.text(
         (text_x + _glow_x, text_y - _glow_y),
         text=map_name,
         font=font,
         fill=(255, 255, 255),
+        stroke_width=2,
     )
     draw_glow.text(
         (text_x - _glow_x, text_y + _glow_y),
         text=map_name,
         font=font,
         fill=(255, 255, 255),
+        stroke_width=2,
     )
     glow_img = glow_img.filter(ImageFilter.BoxBlur(7))
 
