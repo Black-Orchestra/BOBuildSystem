dfa81f22778e0ba2cb425f2df21a386086ab92f1
Tuomo Kriikkula <tuokri@tuta.io>
2025-01-13 21:56:51 +0200

    Fix circular import

--- bobuild/log.py
+++ bobuild/log.py
@@ -1,16 +1,32 @@
 import inspect
 import logging
+import os
 from pathlib import Path
+from typing import TypeVar
+from typing import cast
 
 from loguru import logger
-
-from bobuild.utils import get_var
 
 # TODO: use logrotate on Linux if we run this as a service?
 
 # logger.remove()
 
-_log_dir = Path(get_var("BO_LOG_DIR", ".")).resolve()
+T = TypeVar("T")
+
+_default = object()
+
+
+# NOTE: Duplicated here to avoid circular import.
+def _get_var(name: str, default: T | object = _default) -> str | T:
+    if default is _default:
+        return os.environ[name]
+
+    if name not in os.environ:
+        logger.info("{} not set in environment, using default: '{}'", name, default)
+    return os.environ.get(name, cast(T, default))
+
+
+_log_dir = Path(_get_var("BO_LOG_DIR", ".")).resolve()
 _log_dir.mkdir(parents=True, exist_ok=True)
 
 logger.add(_log_dir / "bobuild.log", rotation="10 MB", retention=5)
