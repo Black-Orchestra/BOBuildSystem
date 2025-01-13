ad9eaf705252f176f2585a19a1f5fb5e06549a3a
Tuomo Kriikkula <tuokri@tuta.io>
2025-01-13 22:39:17 +0200

    Try not to create duplicated firewall rules

--- setup/allow_rs2_server_firewall.ps1
+++ setup/allow_rs2_server_firewall.ps1
@@ -8,30 +8,50 @@ if (-not $env:BO_RS2_SERVER_INSTALL_DIR)
     throw "BO_RS2_SERVER_INSTALL_DIR environment variable is not set!"
 }
 
-$RS2Path = $Env:BO_RS2_SERVER_INSTALL_DIR
-$Description = "Rising Storm 2 Dedicated Server UDP allowed rule."
+$RS2Path = "$Env:BO_RS2_SERVER_INSTALL_DIR\Binaries\Win64\VNGame.exe"
+$Description = "Rising Storm 2 Dedicated Server networking allowed rule."
 
-New-NetFirewallRule -DisplayName "RS2 Dedicated Server Outbound UDP" `
-    -Direction Outbound -Program $RS2Path `
-    -Action Allow -Protocol UDP -Profile Any -Description $Description -Enabled True `
+$Rule = Get-NetFirewallrule -DisplayName "RS2 Dedicated Server Outbound UDP"
+if (!$Rule)
+{
+    New-NetFirewallRule -DisplayName "RS2 Dedicated Server Outbound UDP" `
+        -Direction Outbound -Program $RS2Path `
+        -Action Allow -Protocol UDP -Profile Any -Description $Description -Enabled True
+}
 
-# Game port is 7777, but 7778 is required sometimes too?
-New-NetFirewallRule -DisplayName "RS2 Dedicated Server Inbound UDP 7777-7778" `
-    -Direction Inbound -Program $RS2Path `
-    -Action Allow -Protocol UDP -Profile Any -Description $Description -Enabled True `
-    -RemotePort 7777-7778
+$Rule = Get-NetFirewallrule -DisplayName "RS2 Dedicated Server Inbound UDP 7777-7778"
+if (!$Rule)
+{
+    # Game port is 7777, but 7778 is required sometimes too?
+    New-NetFirewallRule -DisplayName "RS2 Dedicated Server Inbound UDP 7777-7778" `
+        -Direction Inbound -Program $RS2Path `
+        -Action Allow -Protocol UDP -Profile Any -Description $Description -Enabled True `
+        -RemotePort 7777-7778
+}
 
-# Steam A2S query port.
-New-NetFirewallRule -DisplayName "RS2 Dedicated Server Inbound UDP 27015" `
-    -Direction Inbound -Program $RS2Path `
-    -Action Allow -Protocol UDP -Profile Any -Description $Description -Enabled True `
-    -RemotePort 27015
+$Rule = Get-NetFirewallrule -DisplayName "RS2 Dedicated Server Inbound UDP 27015"
+if (!$Rule)
+{
+    # Steam A2S query port.
+    New-NetFirewallRule -DisplayName "RS2 Dedicated Server Inbound UDP 27015" `
+        -Direction Inbound -Program $RS2Path `
+        -Action Allow -Protocol UDP -Profile Any -Description $Description -Enabled True `
+        -RemotePort 27015
+}
 
-New-NetFirewallRule -DisplayName "RS2 Dedicated Server Outbound TCP" `
-    -Direction Outbound -Program $RS2Path `
-    -Action Allow -Protocol TCP -Profile Any -Description $Description -Enabled True `
+$Rule = Get-NetFirewallrule -DisplayName "RS2 Dedicated Server Outbound TCP"
+if (!$Rule)
+{
+    New-NetFirewallRule -DisplayName "RS2 Dedicated Server Outbound TCP" `
+        -Direction Outbound -Program $RS2Path `
+        -Action Allow -Protocol TCP -Profile Any -Description $Description -Enabled True
+}
 
-New-NetFirewallRule -DisplayName "RS2 Dedicated Server Inbound TCP 8080" `
+$Rule = Get-NetFirewallrule -DisplayName "RS2 Dedicated Server Inbound TCP 8080"
+if (!$Rule)
+{
+    New-NetFirewallRule -DisplayName "RS2 Dedicated Server Inbound TCP 8080" `
     -Direction Inbound -Program $RS2Path `
     -Action Allow -Protocol TCP -Profile Any -Description $Description -Enabled True `
     -RemotePort 8080
+}
