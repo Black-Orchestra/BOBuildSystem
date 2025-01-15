0f0020a69216b980cd397553380ac4038e6aa817
Tuomo Kriikkula <tuokri@tuta.io>
2025-01-15 16:34:06 +0200

    Add discord module

---
+++ bobuild/bo_discord.py
@@ -0,0 +1,88 @@
+import argparse
+import asyncio
+import datetime
+
+import aiohttp
+import discord
+import ujson
+
+from bobuild.config import DiscordConfig
+from bobuild.log import logger
+
+
+async def test_builds_webhook(cfg: DiscordConfig):
+    await send_webhook(
+        url=cfg.builds_webhook_url,
+        embed_title="Build start test message!",
+        embed_description="This is a test message!",
+        embed_color=discord.Color.light_embed(),
+        embed_timestamp=datetime.datetime.now(tz=datetime.timezone.utc),
+    )
+
+
+async def test_builds_webhook_rate_limit(cfg: DiscordConfig):
+    for x in range(50):
+        await send_webhook(
+            url=cfg.builds_webhook_url,
+            embed_color=discord.Color.random(),
+            embed_title=f"test_{x}",
+            embed_description=f"test_{x}",
+        )
+
+
+async def send_webhook(
+        url: str,
+        content: str | None = None,
+        embed_color: discord.Color | None = None,
+        embed_title: str | None = None,
+        embed_description: str | None = None,
+        embed_timestamp: datetime.datetime | None = None,
+        embed_footer: str | None = None,
+        fields: list[tuple[str, str, bool]] | None = None,
+) -> None:
+    content = content or ""
+
+    embed = discord.Embed(
+        title=embed_title,
+        description=embed_description,
+        color=embed_color,
+        timestamp=embed_timestamp,
+    )
+    embed.set_footer(text=embed_footer)
+    if fields:
+        for field in fields:
+            embed.add_field(name=field[0], value=field[1], inline=field[2])
+
+    async with aiohttp.ClientSession(
+            json_serialize=ujson.dumps,
+    ) as session:
+        webhook: discord.Webhook = discord.Webhook.from_url(url, session=session)
+        await webhook.send(
+            content=content,
+            embed=embed,
+        )
+
+
+async def main():
+    ap = argparse.ArgumentParser()
+    cfg = DiscordConfig()
+
+    action_choices = {
+        "test_builds_webhook": test_builds_webhook,
+        "test_builds_webhook_rate_limit": test_builds_webhook_rate_limit,
+    }
+    ap.add_argument(
+        "action",
+        choices=action_choices.keys(),
+        help="action to perform",
+    )
+
+    args = ap.parse_args()
+    action = args.action
+    logger.info("performing action: {}", action)
+    await action_choices[args.action](cfg)
+    logger.info("exiting")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
