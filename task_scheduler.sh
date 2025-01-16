d68c84162a1f71a7572c9940f4a38db294c6fc48
Tuomo Kriikkula <tuokri@tuta.io>
2025-01-16 20:21:35 +0200

    Run task scheduler from module dir

--- task_scheduler.sh
+++ task_scheduler.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+# Doing this here again dynamically just to be safe.
+# This is also hard-coded in the Dockerfile.
 user_site=$(python -m site --user-site)
+echo "user_site=${user_site}"
 export PATH="$PATH:${user_site}"
 
+module_path=$(python -c "from pathlib import Path; import bobuild; print(Path(bobuild.__file__).parent)")
+echo "module_path=${module_path}"
+cd "${module_path}" || exit
+
 taskiq scheduler bobuild.tasks:scheduler \
- --tasks-pattern "${user_site}/**/bobuild/**/tasks*.py" \
+ --tasks-pattern "**/tasks*.py" \
  --fs-discover
