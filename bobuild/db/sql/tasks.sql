204fbd4038d3937c7f0db015031f47f434c25be1
Tuomo Kriikkula <tuokri@tuta.io>
2025-01-26 03:01:49 +0200

    Update DB design

--- bobuild/db/sql/tasks.sql
+++ bobuild/db/sql/tasks.sql
@@ -2,8 +2,11 @@
 -- in taskiq_result table, where result_id points to.
 CREATE TABLE IF NOT EXISTS "task"
 (
-    id                 TEXT        NOT NULL PRIMARY KEY,
-    start_time         TIMESTAMPTZ NOT NULL,
-    result_id          TEXT,
-    discord_message_id BIGINT
+    task_id    TEXT        NOT NULL PRIMARY KEY,
+    start_time TIMESTAMPTZ NOT NULL,
+    result_id  TEXT,
+
+    CONSTRAINT fk_result_id
+        FOREIGN KEY (result_id)
+            REFERENCES taskiq_result (task_id)
 );
