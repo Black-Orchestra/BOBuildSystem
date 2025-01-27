f899ddead881733fea6b73a161e748db8c7c2efc
Tuomo Kriikkula <tuokri@tuta.io>
2025-01-27 13:54:16 +0200

    Redesign database

--- bobuild/db/sql/tasks.sql
+++ bobuild/db/sql/tasks.sql
@@ -1,12 +1,33 @@
 -- The actual result of the task is stored by taskiq
 -- in taskiq_result table, where result_id points to.
-CREATE TABLE IF NOT EXISTS "task"
+CREATE TABLE IF NOT EXISTS "build_task"
 (
-    task_id    TEXT        NOT NULL PRIMARY KEY,
-    start_time TIMESTAMPTZ NOT NULL,
-    result_id  TEXT,
+    task_id          TEXT        NOT NULL PRIMARY KEY,
+    start_time       TIMESTAMPTZ NOT NULL,
+    build_result_id  TEXT,
+    git_hash         TEXT,
+    hg_packages_hash TEXT,
+    hg_maps_hash     TEXT,
 
     CONSTRAINT fk_result_id
-        FOREIGN KEY (result_id)
+        FOREIGN KEY (build_result_id)
+            REFERENCES taskiq_result (task_id)
+
+);
+
+CREATE TABLE IF NOT EXISTS "workshop_upload_task"
+(
+    task_id                   TEXT        NOT NULL PRIMARY KEY,
+    start_time                TIMESTAMPTZ NOT NULL,
+    workshop_upload_result_id TEXT,
+    git_hash                  TEXT,
+    git_tag                   TEXT,
+    hg_packages_hash          TEXT,
+    hg_packages_tag           TEXT,
+    hg_maps_hash              TEXT,
+    hg_maps_tag               TEXT,
+
+    CONSTRAINT fk_workshop_upload_result_id
+        FOREIGN KEY (workshop_upload_result_id)
             REFERENCES taskiq_result (task_id)
 );
