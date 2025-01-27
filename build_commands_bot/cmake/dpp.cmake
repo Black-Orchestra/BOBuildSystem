5c12bcd64f1678f05ef86f166665d4325c0210cb
Tuomo Kriikkula <tuokri@tuta.io>
2025-01-28 00:44:06 +0200

    Enable more DPP options

--- build_commands_bot/cmake/dpp.cmake
+++ build_commands_bot/cmake/dpp.cmake
@@ -3,5 +3,7 @@ set(RUN_LDCONFIG 0)
 set(DPP_BUILD_TEST 0)
 set(DPP_NO_VCPKG ON)
 set(DPP_CORO ON)
+set(DPP_FORMATTERS ON)
+set(DPP_NO_CONAN ON)
 
 add_subdirectory("${CMAKE_SOURCE_DIR}/submodules/dpp")
